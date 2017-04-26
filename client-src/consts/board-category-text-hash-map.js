5cb80c6fb3b4199686b977c3f52eb5a59707d866
ricky <ricky3andika@gmail.com>
2017-04-26 23:08:27 +0700

    Board category hash map

---
+++ client-src/consts/board-category-text-hash-map.js
@@ -0,0 +1,10 @@
+const boardCategoryTextHashMap = {
+  'top-board' : 'Top Board',
+  'new-board' : 'Board Baru',
+  'my-board' : 'Board Saya',
+  'bookmarked-board' : 'Board Ditandai',
+  'collaborated-board' : 'Board Kolaborasi',
+  'profile' : 'Profile'
+}
+
+module.exports = boardCategoryTextHashMap;
