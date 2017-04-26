af1db0161bd813cee1645ee19189e79e3d0b5002
ricky <ricky3andika@gmail.com>
2017-04-26 23:12:53 +0700

    Karma configuration file

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,53 @@
-// Karma configuration
-// Generated on Mon Mar 06 2017 15:36:54 GMT+0700 (WIB)
+const stringify = require('stringify');
 
-module.exports = function(config) {
+module.exports = config => {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'sinon', 'browserify'],
-
-
-    // list of files / patterns to load in the browser
     files: [
-        './client-src/tests/**/*Spec.js'
+        './client-src/tests/**/*Spec.js',
     ],
-
-
-    // list of files to exclude
     exclude: [
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    browserify: {
+      configure: bundle => {
+        bundle.once('prebundle', () => {
+          bundle
+            .transform("babelify", { presets: ["es2015"]})
+            .transform(stringify, {
+                appliesTo: { includeExtensions: ['.html'] }
+            });
+        });
+      }
+    },
     preprocessors: {
         'client-src/**/*.js' : ['browserify']
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
+    concurrency: Infinity,
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          },
+        },
+        flags: ['--load-images=true'],
+        debug: true
+      }
+    },
+ 
+    phantomjsLauncher: {
+      exitOnResourceError: true
+    }
+  });
+};
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
