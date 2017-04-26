bc9a14dc32e6be87ae120c3b3e284fa18f1b1fcf
ricky <ricky3andika@gmail.com>
2017-04-26 23:05:54 +0700

    Card type hash map

--- client-src/consts/card-type-text-hash-map.js
+++ client-src/consts/card-type-text-hash-map.js
@@ -1,10 +1,10 @@
-var cardTypeTextHashMap = {
-	'premise' : 'Premis (Premise)',
-	'contention' : 'Kesimpulan (Contention)',
-	'objection' : 'Keberatan (Objection)',
-	'rebuttal' : 'Bantahan (Rebuttal)',
-	'co-premise' : 'Sub Premis (Co Premise)',
-	'co-premise-connector' : ''
+const cardTypeTextHashMap = {
+  'premise' : 'Premis (Premise)',
+  'contention' : 'Kesimpulan (Contention)',
+  'objection' : 'Keberatan (Objection)',
+  'rebuttal' : 'Bantahan (Rebuttal)',
+  'co-premise' : 'Sub Premis (Co Premise)',
+  'co-premise-connector' : ''
 }
 
 module.exports = cardTypeTextHashMap;
