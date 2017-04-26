600c4e832bbb3509b6f3e98fe439a845581f8233
ricky <ricky3andika@gmail.com>
2017-04-26 23:12:39 +0700

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,121 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var util = require('gulp-util');
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const util = require('gulp-util');
+const babel = require('gulp-babel');
+const stringify = require('stringify');
 
-var entryFiles = [
-	{
-		entryFolder : './client-src/',
-		fileName : 'index.js'
-	}
+const entryFiles = [
+  {
+    entryFolder : './client-src/',
+    fileName : 'index.js'
+  },
+  {
+   entryFolder : './client-src/',
+   fileName : 'board-desktop.js'
+  },
+  {
+   entryFolder : './client-src/',
+   fileName : 'board-mobile.js'
+  },
+  {
+    entryFolder : './client-src/',
+    fileName : 'index-mobile.js'
+  }
 ];
 
-var bundleDevelopment = function(entryFile) {
-	var src = entryFile.entryFolder + entryFile.fileName;
+const bundleDevelopment = function(entryFile) {
+  const src = entryFile.entryFolder + entryFile.fileName;
 
-	return browserify({
-		entries : src,
-		debug : true
-	})
-	.bundle()
-	.pipe(source(entryFile.fileName))
-	.pipe(buffer())
-	.pipe(sourcemaps.init({
-		loadMaps : true
-	}))
-	.pipe(uglify())
-	.on('error', util.log)
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('./public/builds/js'));
+  return browserify({
+    entries : src,
+    debug : true
+  })
+  .transform("babelify", {presets: ["es2015"]})
+  .transform(stringify, {
+      appliesTo: { includeExtensions: ['.html'] }
+  })
+  .bundle()
+  .pipe(source(entryFile.fileName))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({
+    loadMaps : true
+  }))
+  .pipe(uglify())
+  .on('error', util.log)
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./public/builds/js'));
 }
 
-var bundleProduction = function(entryFile) {
-	var src = entryFile.entryFolder + entryFile.fileName;
+const bundleProduction = function(entryFile) {
+  const src = entryFile.entryFolder + entryFile.fileName;
 
-	return browserify({
-		entries : src,
-		debug : true
-	})
-	.bundle()
-	.pipe(source(entryFile.fileName))
-	.pipe(buffer())
-	.pipe(uglify())
-	.on('error', util.log)
-	.pipe(gulp.dest('./public/builds/js'));	
+  return browserify({
+    entries : src,
+    debug : true
+  })
+  .transform("babelify", {presets: ["es2015"]})
+  .transform(stringify, {
+      appliesTo: { includeExtensions: ['.html'] }
+  })
+  .bundle()
+  .pipe(source(entryFile.fileName))
+  .pipe(buffer())
+  .pipe(uglify())
+  .on('error', util.log)
+  .pipe(gulp.dest('./public/builds/js')); 
 }
 
-var getBundleName = function() {
-	var packageJson = require('./package.json');
-	var version = packageJson.version;
-	var name = packageJson.name;
+const getBundleName = function() {
+  const packageJson = require('./package.json');
+  const version = packageJson.version;
+  const name = packageJson.name;
 
-	return version + '.' + name + '.' + 'min';
+  return version + '.' + name + '.' + 'min';
 }
 
 gulp.task('build-scripts-development', function() {
-	entryFiles.forEach(function(entryFile) {
-		bundleDevelopment(entryFile)
-	})
+  entryFiles.forEach(function(entryFile) {
+    bundleDevelopment(entryFile)
+  })
 })
 
 gulp.task('build-scripts-production', function() {
-	entryFiles.forEach(function(entryFile) {
-		bundleProduction(entryFile)
-	})
+  entryFiles.forEach(function(entryFile) {
+    bundleProduction(entryFile)
+  })
 })
 
+gulp.task('copy-css', function() {
+  gulp.src([
+    './node_modules/bootstrap/dist/css/bootstrap.css',
+    './node_modules/bootstrap.native/assets/css/bootstrap.min.css',
+    './node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css',
+    './client-src/lib-styles/grab-to-pan.css',
+    './node_modules/nprogress/nprogress.css',
+    './node_modules/alertifyjs/build/css/alertify.min.css',
+    './node_modules/alertifyjs/build/css/themes/bootstrap.rtl.css',
+    './node_modules/@pirxpilot/tags-input/tags-input.css',
+    //'./node_modules/taggle/example/css/taggle.css',
+    './client-src/lib-styles/smt-bootstrap.css',
+    './client-src/images/circle-loading.gif'
+  ])
+  .pipe(gulp.dest('./public/builds/css'))
+})
+
+gulp.task('copy-fonts', function() {
+  gulp.src([
+    './node_modules/bootstrap/dist/fonts/*.*'
+  ])
+  .pipe(gulp.dest('./public/builds/fonts'))
+})
 
 gulp.task('build-styles', function() {
 
 })
 
 gulp.task('default', ['build-scripts-development']);
+gulp.task('copy-assets', ['copy-css', 'copy-fonts']);
