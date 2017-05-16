aa88aa83447df0038e30c318bb1bdc97eff0dc71
RickyAndi <ricky3andika@gmail.com>
2017-05-16 18:53:57 +0700

    Model register function

---
+++ server-src/models/require-models.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+  require('./card');
+  require('./arguer-requests');
+  require('./board');
+  require('./board-arguers');
+  require('./bookmarks');
+  require('./rejected-arguer-requests');
+  require('./tag');
+  require('./user');
+}
