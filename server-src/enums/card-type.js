09c38dc01d3cf13685a9634bc48042b2b2c1311c
RickyAndi <ricky3andika@gmail.com>
2017-05-16 18:53:16 +0700

    Card type enum

---
+++ server-src/enums/card-type.js
@@ -0,0 +1,10 @@
+const cardType = {
+  'CONTENTION' : 'contention',
+  'REASON' : 'reason',
+  'OBJECTION' : 'objection',
+  'REBUTTAL' : 'rebuttal',
+  'CO-REASON-CONNECTOR' : 'co-reason-connector',
+  'CO-REASON' : 'co-reason'
+};
+
+module.exports = cardType;
