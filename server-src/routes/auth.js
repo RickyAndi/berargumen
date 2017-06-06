55702731cf64452dd364c838d9b68606a5aff81b
RickyAndi <ricky3andika@gmail.com>
2017-06-06 10:43:11 +0700

    Add local login

--- server-src/routes/auth.js
+++ server-src/routes/auth.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const router = express.Router();
 const passport = require('../passport');
+const { isNotAuthenticated, isAuthenticated  } = require('../middlewares');
 
 module.exports = () => {
-  router.get('/facebook',  passport.authenticate('facebook', {
+  router.get('/logout', isAuthenticated, (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  router.get('/facebook',  isNotAuthenticated, passport.authenticate('facebook', {
     scope : ['public_profile', 'email']
-  }))
+  }));
 
   router.get('/facebook/callback', passport.authenticate('facebook', {
     failureRedirect : '/',
   }), (req, res) => {
     res.redirect('/')
+  });
+  
+  router.get('/local', (req, res, next) => {
+    req.body = {
+      username : 'jancok',
+      password: 'jancok'
+    }
+    passport.authenticate('local', { failureRedirect : '/'})(req, res, next);
+  },(req, res, next) => {
+    res.redirect('/');
   });
 
   return router;
