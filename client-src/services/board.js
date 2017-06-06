c75cf868c35cad34ea7f6baffc01c910d88a1a24
RickyAndi <ricky3andika@gmail.com>
2017-06-06 10:30:59 +0700

    Add more function

--- client-src/services/board.js
+++ client-src/services/board.js
@@ -2,30 +2,23 @@ const http = require('./http');
 
 module.exports = (() => {
   const urls = {
-    'top' : '/api/board/featured',
     'my' : '/api/board/my',
     'collaborated' : '/api/board/collaborated',
-    'new' : '/api/board/new',
     'bookmarked' : '/api/board/bookmarked',
-    'all' : '/api/board/all'
+    'all' : '/api/board/all',
+    'create' : '/api/board/create',
+    'publish' : '/api/board/publish/',
+    'unpublish' : '/api/board/unpublish/',
+    'upvote' : '/api/board/upvote/',
+    'downvote' : '/api/board/downvote/',
+    'remove-upvote' : '/api/board/remove-upvote/',
+    'remove-downvote' : '/api/board/remove-downvote/',
+    'bookmark' : '/api/board/bookmark',
+    'remove-bookmark' : '/api/board/remove-bookmark',
+    'delete' : '/api/board/delete/',
+    'edit' : '/api/board/edit/'
   }
-
-  function getTop(data) {
-    const query = data == null ? {} : data;
-    return http.get(urls['top'], query)
-      .then(function(response) {
-        return response.data;
-      });
-  }
-
-  function getNew(data) {
-    const query = data == null ? {} : data;
-    return http.get(urls['new'], query)
-      .then(function(response) {
-        return response.data;
-      });
-  }
-
+  
   function getBookmarked(data) {
     const query = data == null ? {} : data;
     return http.get(urls['bookmarked'], query)
@@ -37,7 +30,7 @@ module.exports = (() => {
   function getCollaborated(data) {
     const query = data == null ? {} : data;
     return http.get(urls['collaborated'], query)
-      .then(function(response) {
+      .then((response) => {
         return response.data;
       });
   }
@@ -45,7 +38,7 @@ module.exports = (() => {
   function getMy(data) {
     const query = data == null ? {} : data;
     return http.get(urls['my'], query)
-      .then(function(response) {
+      .then((response) => {
         return response.data;
       });
   }
@@ -53,17 +46,112 @@ module.exports = (() => {
   function getAll(data) {
     const query = data == null ? {} : data;
     return http.get(urls['all'], query)
-      .then(function(response) {
+      .then((response) => {
         return response.data;
       });
   }
 
+  function create(data) {
+    return http.post(urls['create'], data)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        throw error.response;
+      })
+  }
+
+  function publish(boardId) {
+    const url = urls['publish'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function unpublish(boardId) {
+    const url = urls['unpublish'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function upvote(boardId) {
+    const url = urls['upvote'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function downvote(boardId) {
+    const url = urls['downvote'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function removeUpvote(boardId) {
+    const url = urls['remove-upvote'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function removeDownvote(boardId) {
+    const url = urls['remove-downvote'] + boardId; 
+    return http.put(url, {})
+      .then((response) => {
+        return response.data;
+      })
+  }
+  
+  function bookmark(boardId) {
+     return http.post(urls['bookmark'], { boardId : boardId })
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function removeBookmark(boardId) {
+     return http.post(urls['remove-bookmark'], { boardId : boardId })
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function deleteBoard(boardId) {
+    return http.delete(urls['delete'] + boardId)
+      .then((response) => {
+        return response.data;
+      })
+  }
+
+  function editBoard(boardId, data) {
+     return http.put(urls['edit'] + boardId, data)
+      .then((response) => {
+        return response.data;
+      })
+  }
+
   return {
-    getTop : getTop,
     getMy : getMy,
     getCollaborated : getCollaborated,
-    getNew : getNew,
     getBookmarked : getBookmarked,
-    getAll : getAll
+    getAll : getAll,
+    create : create,
+    publish : publish,
+    unpublish : unpublish,
+    upvote : upvote,
+    downvote : downvote,
+    removeUpvote : removeUpvote,
+    removeDownvote : removeDownvote,
+    bookmark : bookmark,
+    removeBookmark : removeBookmark,
+    deleteBoard : deleteBoard,
+    editBoard : editBoard
   }
 })();
