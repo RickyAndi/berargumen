9a3633c7e9f54cea04f6d91f3e42692262039ad2
RickyAndi <ricky3andika@gmail.com>
2017-06-06 10:37:07 +0700

    Shared utils
    - comparing
    - objectNormalizer

---
+++ shared/utils/comparing.js
@@ -0,0 +1,4 @@
+module.exports = {
+  isNull : (value) => value === null,
+  isUndefined : (value) => value === undefined,
+}
---
+++ shared/utils/object-normalizer.js
@@ -0,0 +1,3 @@
+module.exports = (object) => {
+  return JSON.parse(JSON.stringify(object));
+}
