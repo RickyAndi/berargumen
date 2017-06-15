ec65091d80d950dcad45ca0055dd3331c7c37a05
RickyAndi <ricky3andika@gmail.com>
2017-06-15 14:34:58 +0700

    Add route find board by slug

--- server-src/routes/board.js
+++ server-src/routes/board.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const MobileDetect = require('mobile-detect');
+const boardService = require('../services/board');
+const { async, await } = require('asyncawait');
 
 module.exports = sockets => {
-  router.get('/:boardId', (req, res) => {
-    res.render('board-desktop.ejs');
-  });
+  router.get('/:slug', async((req, res) => {
+    const slug = req.params.slug;
+    const board = await(boardService.findBySlug(slug, 'creator'));
+    
+    return res.render('board-desktop.ejs', {
+      boardId: board._id,
+      boardTitle: board.title,
+      creatorName: board.creator.displayName,
+      boardDescription: board.description
+    });
+  }));
 
   router.get('/', (req, res) => {
     const md = new MobileDetect(req.headers['user-agent']);
