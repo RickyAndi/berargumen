860d830bfc14b18425b8b5a74a9cb0da4d818263
RickyAndi <ricky3andika@gmail.com>
2017-06-15 14:34:16 +0700

    Add developmentMode variable in render

--- server-src/routes/index.js
+++ server-src/routes/index.js
@@ -4,18 +4,10 @@ const MobileDetect = require('mobile-detect');
 
 module.exports = sockets => {
   router.get('/', (req, res) => {
-    if(process.env.NODE_ENV === 'development') {
-      return res.render('index-development.ejs');
-    }
-    //res.render('index-mobile.ejs');   
-    // const md = new MobileDetect(req.headers['user-agent']);
-
-    // if(md.mobile()) {
-    //  res.render('index-mobile.ejs'); 
-    // }
-    
-    return res.render('index.ejs');
-  })
+    return res.render('index.ejs', {
+      developmentMode: process.env.NODE_ENV === 'development' ? true : false
+    });
+  });
 
   return router;
 }
